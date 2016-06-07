a682f8addc8a46d6027776a019f4d88678bdd321
Christophe Larsonneur <clarsonneur@gmail.com>
2016-06-07 14:51:06 +0200

    Adapt forj-create to latest design

--- docker/forjj/forjj-create.sh
+++ docker/forjj/forjj-create.sh
@@ -14,7 +14,7 @@ GITHUB_HOST=github.hpe.com
 GITHUB_ORGANIZATION=christophe-larsonneur
 CONTRIB_REPO=forjj-contribs
 
-INFRA_REPO=infra
+INFRA_REPO=christophe-larsonneur-infra
 
 declare -A ORCH
 
@@ -39,16 +39,25 @@ function parse_options
        export https_proxy=$2
        shift;shift;;
      --driver-*)
-       TYPE=${1:10}
+       TYPE=${1:9}
        ORCH[$TYPE]="$2" 
        if [ ! -x /forjj-contribs/$TYPE/$2/$2 ]
        then
-          echo "The '$2' '$TYPE' orchestrator is invalid: Unable to find forjj-contribs/$TYPE/$2/$2 executable"
+          echo "The '$2' '$TYPE' driver is invalid: Unable to find forjj-contribs/$TYPE/$2/$2 executable"
           exit 1
        fi
+       echo "Using '$2' '$TYPE' forjj driver"
        shift;shift;;
+     *)
+      shift;;
    esac
  done  
+}
+
+function json-test
+{
+ [ "$(echo "$1" | jq -r "$2")" = true ]
+ return $?
 }
 
 function check_upstream
@@ -61,84 +70,84 @@ function check_upstream
 # All parameters are passed to the orchestrator for its task
 
 ORCH_SCM=${ORCH[upstream]}
-
-REPO=$1 ; shift
-
-/forjj-contribs/upstream/$ORCH_SCM/$ORCH_SCM upstream_configure $REPO "$@"
+REPO="$1"
+shift
+# ### Checking the remote service
+check_repo="$(/forjj-contribs/upstream/$ORCH_SCM/$ORCH_SCM check "$@")"
+# If all is in place, return must be 0 and state_code = 200
 
 # Check that the SCM upstream orchestrator as set origin and upstream data.
-if [ $? -ne 0 ]
+RET=$?
+if [ $RET -ne 0 ]
 then
-   echo "SCM Upstream orchestrator '$ORCH_SCM' fails with return code $?. forjj aborted."
+   echo "SCM Upstream orchestrator '$ORCH_SCM' fails with return code $RET. forjj aborted."
    exit 1
 fi
 
-if [ ! -d /devops/$REPO/.git ]
+# Check return that service is up and found a repo. But it do not test the infra config file existence.
+# But it gives the config name if i
+if json-test "$check_repo" '.state_code == 200'
 then
-   echo "SCM Upstream orchestrator '$ORCH_SCM' fails to ensure '$REPO' is a git repository. forjj aborted."
-   exit 1
+   return 1
 fi
 
-if [ $(git remote | grep -e "^origin$" -e '^upstream$' | wc -l) -ne 2 ]
-then
-   echo "SCM Upstream orchestrator '$ORCH_SCM' fails to ensure origin and upstream remote properly set in '$REPO' git repository. forjj aborted."
-   exit 1
-fi
 }
 
-echo "Analyzing '$1' repositories..."
+function ensure_local_repo_exist
+{
+ # ### Checking local repositories
+ if [ ! -d /devops/$1/.git ]
+ then
+    git init /devops/$1
+ fi
+}
 
-ORGA_NAME="$(basename "$1")"
-
-shift
+####################### MAIN
 
 cd /devops/
 
 # If we do not found the ci subdir, then we consider that this path is not having a valid 
 # forjj-contribs.
 # this path can be git repo or a simple dir. No git task is done on this repo anyway.
-if [ ! -d /forjj-contrib/ci ]
+if [ ! -d /forjj-contribs/ci ]
 then
-   git clone https://$GITHUB_HOST/$GITHUB_ORGANIZATION/$CONTRIB_REPO /forjj-contrib
+   git clone https://$GITHUB_HOST/$GITHUB_ORGANIZATION/$CONTRIB_REPO /forjj-contribs
 fi
 
-parse_options
-
-
-# TODO: Bug: ORCH not updated
+parse_options "$@"
 
 if [ "${ORCH[upstream]}" = "" ]
 then
-   echo "Using 'github' as default forjj SCM-Upstream orchestrator"
-   ORCH[upstream]=github
+   echo "SCM-Upstream plugin missing. Please use --git-us to set one."
+   exit 1
 fi
 
-if [ "${ORCH[ci]}" = "" ]
+# This check the infra repo, ensure infra is a cloned git copy, and add initial commits if needed.
+if ! check_upstream $INFRA_REPO --infra "$@"
 then
-   echo "Using 'jenkins-ci' as default forjj CI orchestrator"
-   ORCH_SCM=jenkins-ci
+   echo "DevOps Infra solution is already implemented. You cannot use 'create' anymore. To build your workspace, use 'update'."
+   exit 1
 fi
 
-# This will update the infra repo, ensure infra is a cloned git copy, and add initial commits if needed.
-check_upstream $INFRA_REPO --infra "$@"
+ensure_local_repo_exist $INFRA_REPO
+ensure_local_repo_exist ${INFRA_REPO}-state
+
+RES="$(/forjj-contribs/upstream/${ORCH[upstream]}/${ORCH[upstream]} create "$@")"
+
+if ! json-test "$RES" '.state-code == 200'
+then
+   echo "DevOps solution failure. Exiting."
+   exit 1
+fi
 
 # Check about CI service configuration in infra thanks to the CI orchestrator.
 # Ask the CI orchestrator to check if the service is properly configured in `infra`.
 # - Ensure CI code exist and up to date.
-/forjj-contribs/ci/${ORCH[ci]}/${ORCH[ci]} ci_configure "$@"
-
-if [ "$REPOS" != "" ]
-then
-   echo "Ensuring '$REPOS' exists and connected to the upstream..."
-   
-   for REPO in ${REPOS/;/ }
-   do # This will update only infra repository (commit auto-created for each repo.)
-      check_upstream $REPO "$@"
-   done
-fi
+#if [ "${ORCH[ci]}" ! = "" ]
+#then 
+#   /forjj-contribs/ci/${ORCH[ci]}/${ORCH[ci]} update "$@"
+# fi
 
 # Process done!
 # Infra is updated
 
-# The next step should be to maintain the service as described by the infra repository.
-# ie : repos exists, services exists up and running and properly configured.
