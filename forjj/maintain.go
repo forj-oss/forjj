dab7e0f61f89878b0f3a7ec300293db355415fbe
Christophe Larsonneur <clarsonneur@gmail.com>
2016-08-07 21:57:21 +0200

    Fix  go format (gofmt used)

--- forjj/maintain.go
+++ forjj/maintain.go
@@ -1,5 +1,7 @@
 package main
 
+import "gopkg.in/alecthomas/kingpin.v2"
+
 // Call docker to create the Solution source code from scratch with validated parameters.
 // This container do the real stuff (git/call drivers)
 // I would expect to have this go tool to have a do_create to replace the shell script.
@@ -12,6 +14,10 @@ func (a *Forj) Maintain() {
     a.LoadForjjPluginsOptions()
 
     // Loop on each drivers to ask them to maintain the infra, it controls.
+
+    // Ensure remote upstream exists - calling upstream driver - maintain
+    err := a.driver_do("upstream", "maintain") // This will create/update the upstream service
+    kingpin.FatalIfError(err, "Driver create issue. Unable to instantiate requested resources.")
 
     // Loop on upstream repositories to ensure it exists with at least a README.md file.
 
