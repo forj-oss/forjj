c6df7e8000d4095b0d0e2fea79f1792602c06a04
Christophe Larsonneur <clarsonneur@gmail.com>
2018-02-02 17:00:15 +0100

    Added `concatenate` in template functions. data added printout improved.

--- flow/flow_task_set.go
+++ flow/flow_task_set.go
@@ -12,7 +12,9 @@ type FlowTaskSet map[string]map[string]forjfile.ForjValues
 
 func (fts FlowTaskSet)apply(tmpl_data *FlowTaskModel, Forjfile *forjfile.Forge) error {
 	tmpl := template.New("flow-set")
-	funcs := template.FuncMap{}
+	funcs := template.FuncMap{
+		"concatenate": fmt.Sprint,
+	}
 	for object_name, object_data := range fts {
 		for instance_name, instance_data := range object_data {
 			if v, err := utils.Evaluate(instance_name, tmpl, tmpl_data, funcs) ; err != nil {
@@ -22,7 +24,7 @@ func (fts FlowTaskSet)apply(tmpl_data *FlowTaskModel, Forjfile *forjfile.Forge) 
 			}
 			if len(instance_data) == 0 {
 				Forjfile.Set(object_name, instance_name, "", "")
-				gotrace.Trace("instance '%s/%s' added with no keys.", object_name, instance_name)
+				gotrace.Trace("'%s/%s: {}' added.", object_name, instance_name)
 				continue
 			}
 			for key, value:= range instance_data {
@@ -35,7 +37,13 @@ func (fts FlowTaskSet)apply(tmpl_data *FlowTaskModel, Forjfile *forjfile.Forge) 
 					return fmt.Errorf("Unable to evaluate instance '%s' key '%s' value '%s'. %s", instance_name, key, value, err)
 				} else {
 					Forjfile.Set(object_name, instance_name, key, v)
-					gotrace.Trace("instance '%s/%s' key '%s' value '%s' added.", object_name, instance_name, key, value.Get())
+					if v == "" {
+						gotrace.Trace("'%s/%s: {}' added. '%s/%s/%s' deleted.",
+							object_name, instance_name, object_name, instance_name, key)
+					} else {
+						gotrace.Trace("'%s/%s/%s=\"%s\"' added.", object_name, instance_name, key, v)
+					}
+
 				}
 			}
 		}
