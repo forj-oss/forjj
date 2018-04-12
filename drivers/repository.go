d3ca03ef44e45d5e92352f46c53373455b4e0ec6
Christophe Larsonneur <clarsonneur@gmail.com>
2018-04-12 22:27:29 +0200

    Fix git add for deploy repos

--- drivers/repository.go
+++ drivers/repository.go
@@ -32,7 +32,7 @@ func (d *Driver) GitAddPluginFiles(moveTo func(string) (string, error)) error {
 		if err := RunInPath(where, moveTo, func() error {
 			gotrace.Trace("GIT: Adding %s %d files related to '%s'", where, len(files), d.Plugin.Result.Data.CommitMessage)
 
-			return d.gitAddPluginFiles(files)
+			return d.gitAddPluginFiles(where, files)
 		}); err != nil {
 			return err
 		}
@@ -60,13 +60,17 @@ func RunInPath(where string, moveTo func(string) (string, error), runIn func() e
 	return nil
 }
 
-func (d *Driver) gitAddPluginFiles(files []string) error {
+func (d *Driver) gitAddPluginFiles(where string, files []string) error {
 	if files == nil {
 		return nil
 	}
 	fileToAdd := make([]string, len(files))
 	for iCount, file := range files {
-		fileToAdd[iCount] = path.Join("apps", d.DriverType, file)
+		if where == goforjj.FilesSource {
+			fileToAdd[iCount] = path.Join("apps", d.DriverType, file)
+		} else {
+			fileToAdd[iCount] = file
+		}
 	}
 	if i := git.Add(fileToAdd); i > 0 {
 		return fmt.Errorf("Issue while adding code to git. RC=%d", i)
