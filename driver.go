45ccefefcf18e0752615ddff98c33f327a798887
Christophe Larsonneur <clarsonneur@gmail.com>
2018-10-03 13:21:02 +0200

    Fix build issue

--- driver.go
+++ driver.go
@@ -192,7 +192,7 @@ func (a *Forj) driver_do(d *drivers.Driver, instance_name, action string, args .
 		d.Plugin.PluginSetWorkspaceMount(a.w.Path())
 		d.Plugin.PluginSetDeploymentMount(a.d.GetReposPath())
 	} else {
-		d.Plugin.PluginSetDefaultMount()
+		d.Plugin.SetDefaultMounts()
 	}
 	d.Plugin.PluginSetSource(path.Join(a.i.Path(), "apps", d.DriverType))
 	d.Plugin.PluginSetDeployment(a.d.GetReposPath())
